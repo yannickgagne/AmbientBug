0463ce697b110784f8f9310b3b1e79fdf4f0d697
Yannick Gagne <yannickgagne@users.noreply.github.com>
2022-03-08 12:09:04 -0500

    add thing on display to show that mcu is alive

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ from machine import Pin, I2C
 import ahtx0
 import ssd1306
 
-last_tick = time.ticks_ms()
+pub_last_tick = 0
 pub_delay_ms = 60000
+oc_last_tick = 0
+oc_delay_ms = 2000
+oc = 0
 
 #Sensor setup
 i2c = I2C(scl=Pin(5), sda=Pin(4))
 sensor = ahtx0.AHT20(i2c)
 oled = ssd1306.SSD1306_I2C(128, 32, i2c)
 time.sleep_ms(20)
+oled.rotate(False)
 
 #MQTT Cayenne setup
 mqtt_user = "qs8$6meFa%D^1Gzw1o9^S5i4plYhPB$q"
@@ -29,34 +33,50 @@ client = MQTTClient(mqtt_client_id, mqtt_server, port=mqtt_port, user=mqtt_user,
 ssid = "57xxIoT"
 password =  "jx+h2:s^4;)e)r-;+8[e3[39(rq5]>0_.(};)gu@3&nkn&d$2,gr,@o{{&g?"
 
-station = network.WLAN(network.STA_IF)
+st_if = network.WLAN(network.STA_IF)
+ap_if = network.WLAN(network.AP_IF)
  
-if station.isconnected() == True:
+if st_if.isconnected() == True:
   print("Already connected")
  
-station.active(True)
-station.connect(ssid, password)
+st_if.active(True)
+ap_if.active(False)
+st_if.connect(ssid, password)
  
-while station.isconnected() == False:
+while st_if.isconnected() == False:
   pass
  
 print("Connection successful")
-print(station.ifconfig())
+print(st_if.ifconfig())
 
 MQTT_GO = True
 #Publish dummy data to Cayenne
 if MQTT_GO == True:
   #client.connect()
   while True:
-    if time.ticks_diff(time.ticks_ms(), last_tick) > pub_delay_ms:
-      last_tick = time.ticks_ms()
+    if time.ticks_diff(time.ticks_ms(), oc_last_tick) > oc_delay_ms: #loop 1 time per 2 seconds
+      if oc == 0:
+        oled.text("|", 120, 0, 1)
+      if oc == 1:
+        oled.text("/", 120, 0, 1)
+      if oc == 2:
+        oled.text("-", 120, 0, 1)
+      if oc == 3:
+        oled.text("\", 120, 0, 1)
+      oc_last_tick = time.ticks_ms()
+      oled.show()
+      oc += 1
+      if oc > 3:
+        oc = 0
+        
+    if time.ticks_diff(time.ticks_ms(), pub_last_tick) > pub_delay_ms: #loop 1 time per minute
+      pub_last_tick = time.ticks_ms()
       #Get temp/humi from sensor
       stemp = sensor.temperature
       shumi = sensor.relative_humidity
       print("Temperature: %0.2f C" % stemp)
       print("Humidity: %0.2f %%" % shumi)
       #OLED
-      oled.rotate(False)
       oled.fill(0)
       oled.text('%0.2f C' % stemp, 0, 0, 1)
       oled.text('%0.2f %%' % shumi, 0, 16, 1)
